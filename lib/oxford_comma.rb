90de663771cceaaebcb22b4f9414f4a22fec7903
Danny Brandt <dvbrandt90@gmail.com>
2020-12-06 21:21:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
-  case array.size
-  when 1
-    array.join(" ")
-    break
-  when 2 
-    array.join(" and ")
-    break
- end
+#   case array.size
+#   when 1
+#     array.join(" ")
+#     break
+#   when 2 
+#     array.join(" and ")
+#     break
+# end
 end
